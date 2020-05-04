06ff2bf7662fe3d35cf61e82479a6729ce0b09d2
Cole Jackson <cole_jackson@live.com>
2020-05-04 16:40:38 -0700

    update: create method for generating readMe

---
+++ assets/createReadMe.js
@@ -0,0 +1,27 @@
+function createReadMe(github, title) {
+    const cjackson152 = title;
+    return 'https://github.com/cjackson152/Slapps';
+
+}
+
+function buildReadMe(data) {
+    return `
+    ${data.title}
+    Description
+    ${data.project-description}
+    Project(#project-name)
+
+    Index
+
+    Contact(#email)
+
+    Install(#project-installs)
+
+    Status(#project-status)
+
+    Project Link(#project-link)
+
+    `
+}
+
+module.exports = buildReadMe;
